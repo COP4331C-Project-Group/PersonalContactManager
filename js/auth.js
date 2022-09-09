4e4d8b0ebab40e669bf7e71cc2c69293fba6e5b0
Nashir Janmohamed <nashirbass@gmail.com>
2022-09-09 17:44:48 -0400

    Add js for storing/reading cookie for user authentication

--- js/auth.js
+++ js/auth.js
@@ -153,3 +153,50 @@ function doLogout()
   document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
   window.location.href = "auth.html";
 }
+
+function saveCookie()
+{
+  let minutes = 20;
+  let date = new Date();
+  date.setTime(date.getTime()+(minutes*60*1000)); 
+  document.cookie = "firstName=" + window.firstName + ",lastName=" + window.lastName + ",userId=" + window.userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+  userId = -1;
+  let data = document.cookie;
+  let splits = data.split(",");
+  for(var i = 0; i < splits.length; i++) 
+  {
+    let thisOne = splits[i].trim();
+    let tokens = thisOne.split("=");
+    if( tokens[0] == "firstName" )
+    {
+      window.firstName = tokens[1];
+    }
+    else if( tokens[0] == "lastName" )
+    {
+      window.lastName = tokens[1];
+    }
+    else if( tokens[0] == "userId" )
+    {
+      window.userId = parseInt( tokens[1].trim() );
+    }
+  }
+  
+  if( window.userId < 0 )
+  {
+    if (window.location.pathname.split("/").pop() !== "auth.html") {
+      window.location.href = "auth.html";
+    }
+  }
+  else
+  {
+    if (window.location.pathname.split("/").pop() !== "index.html") {
+      window.location.href = "index.html";
+    }
+
+    document.getElementById("userName").innerHTML = "Logged in as " + window.firstName + " " + window.lastName;
+  }
+}
