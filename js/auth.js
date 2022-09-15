42ee1a586d5886c3b7922c0ceb7dfe04aaf88a04
Nashir Janmohamed <nashirbass@gmail.com>
2022-09-15 17:52:03 -0400

    Abstract code into helper methods and refactor auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,3 @@
-// TODO: abstract shared code (e.g. request sending); can pass a lambda
-
 var confirmPasswordString = document.getElementById("registerConfirmPassword");
 confirmPasswordString.addEventListener("keydown", function (e) {
   if (e.code === "Enter") {
@@ -28,6 +26,29 @@ function changeRegisterToLogin(){
   document.getElementById("title").innerHTML = "Log In";
 }
 
+function saveUserInfo(userJson) {
+  window.userID = userJson.ID;
+
+  window.firstName = userJson.firstName;
+  window.lastName = userJson.lastName;
+
+  saveCookie();
+}
+
+function validateLoginForm(username, password) {
+  if (username.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must provide a username!";
+    return false;
+  }
+
+  if (password.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must provide a password!";
+    return false;
+  }
+
+  return true;
+}
+
 async function doLogin() {
   window.userID = 0;
   window.firstName = "";
@@ -36,52 +57,78 @@ async function doLogin() {
   let username = document.getElementById("loginUsername").value;
   let password = document.getElementById("loginPassword").value;
 
-  // Check that all fields are populated
-  if (username.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must provide a username!";
+  if (!validateLoginForm(username, password)) {
     return;
   }
 
-  if (password.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must provide a password!";
-    return;
-  }
   document.getElementById("authResult").innerHTML = "";
 
-  let tmp = {username:username, password:password};
   // TODO: switch to using hashes after getting everything working
-  // var hash = md5( password );
-  // var tmp = {username:username, password:hash};
-  let jsonPayload = JSON.stringify( tmp );
+  // var passwordHash = md5( password );
 
-  var url = window.urlBase + '/users/Login' + window.extension + "?";
-  for ( var key in tmp ) {
-    url += key + "=" + tmp[key] + "&";
-  }
+  const [status, responseJson] = await getData(
+    window.urlBase + '/users/Login' + window.extension + "?",
+    {
+      username:username,
+      password:password
+    });
 
-  let response = await fetch(url);
-  let responseJson = await response.json();
-  if (response.status == 200) {
-    window.userID = responseJson.data.userID;
-
-    window.firstName = responseJson.data.firstName;
-    window.lastName = responseJson.data.lastName;
-
-    saveCookie();
-
+  if (status == 200) {
+    saveUserInfo(responseJson.data);
     window.location.href = "index.html";
   } else {
     document.getElementById("authResult").innerHTML = responseJson.status_message;
   }
 }
 
+function validateRegistrationForm(firstName, lastName, username, password, confirmPassword) {  
+  // TODO: update this later if needed/add more constraints
+  const minimumPasswordLength = 6;
+
+  // Check that all fields are populated
+  if (firstName.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must provide a first name!";
+    return false;
+  }
+
+  if (lastName.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must provide a last name!";
+    return false;
+  }
+
+  if (username.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must provide a username!";
+    return false;
+  }
+
+  if (password.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must provide a password!";
+    return false;
+  }
+
+  if (confirmPassword.length == 0) {
+    document.getElementById("authResult").innerHTML = "Must confirm password!";
+    return false;
+  }
+
+  // TODO: add better handling of strong password here
+  if (password.length < minimumPasswordLength) {
+    document.getElementById("authResult").innerHTML = "Please choose a stronger password (min password length = 6)";
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    document.getElementById("authResult").innerHTML = "Passwords must match!";
+    return false;
+  }
+
+  return true;
+}
+
 async function doRegister() {
   window.userID = 0;
   window.firstName = "";
   window.lastName = "";
-
-  // TODO: update this later if needed/add more constraints
-  const minimumPasswordLength = 6;
   
   let firstName = document.getElementById("registerFirstName").value;
   let lastName = document.getElementById("registerLastName").value;
@@ -89,80 +136,26 @@ async function doRegister() {
   let password = document.getElementById("registerPassword").value;
   let confirmPassword = document.getElementById("registerConfirmPassword").value;
 
-  // Check that all fields are populated
-  if (firstName.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must provide a first name!";
-    return;
-  }
-
-  if (lastName.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must provide a last name!";
-    return;
-  }
-
-  if (username.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must provide a username!";
-    return;
-  }
-
-  if (password.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must provide a password!";
-    return;
-  }
-
-  if (confirmPassword.length == 0) {
-    document.getElementById("authResult").innerHTML = "Must confirm password!";
-    return;
-  }
-
-  // TODO: add better handling of strong password here
-  if (password.length < minimumPasswordLength) {
-    document.getElementById("authResult").innerHTML = "Please choose a stronger password (min password length = 6)";
-    return;
-  }
-
-  if (password !== confirmPassword) {
-    document.getElementById("authResult").innerHTML = "Passwords must match!";
+  if (!validateRegistrationForm(firstName, lastName, username, password, confirmPassword)) {
     return;
   }
 
   document.getElementById("authResult").innerHTML = "";
 
-  let tmp = {
-    firstName:firstName,
-    lastName:lastName,
-    username:username,
-    password:password
-  };
   // TODO: switch to using hashes after getting everything working
-  // var hash = md5( password );
-  // var tmp = {firstName:firstName, lastName:lastName, username:username, password:hash};
-  let jsonPayload = JSON.stringify( tmp );
+  // var passwordHash = md5( password );
 
-  let url = window.urlBase + '/users/Register' + window.extension;
+  const [status, responseJson] = await postData(
+    window.urlBase + '/users/Register' + window.extension,
+    {
+      firstName:firstName,
+      lastName:lastName,
+      username:username,
+      password:password
+    });
 
-  var formData = new FormData()
-  for ( var key in tmp ) {
-    formData.append(key, tmp[key]);
-  }
-  const requestOptions = {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json'
-          },
-          body: formData
-      };
-
-  let response = await fetch(url, requestOptions);
-  let responseJson = await response.json();
-  if (response.status == 201) {
-    window.userID = responseJson.data.userID;
-
-    window.firstName = responseJson.data.firstName;
-    window.lastName = responseJson.data.lastName;
-
-    saveCookie();
-
+  if (status == 201) {
+    saveUserInfo(responseJson.data);
     window.location.href = "index.html";
   } else {
     document.getElementById("authResult").innerHTML = responseJson.status_message;
