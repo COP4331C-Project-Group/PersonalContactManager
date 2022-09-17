261c5070d27875679398072c438b03294f89a9f8
Michael <38502074+mplekunov@users.noreply.github.com>
2022-09-17 14:03:46 -0400

    Add semicolon

--- API/users/Login.php
+++ API/users/Login.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('error_reporting', E_ALL);
+    
     require_once __DIR__ . '/../utils/JsonUtils.php';
     require_once __DIR__ . '/../utils/ResponseSender.php';
     require_once __DIR__ . '/../utils/RequestReceiver.php';
