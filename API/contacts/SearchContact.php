4ef522e821b599a0a0e4f4705cec3f84233d110f
Michael <38502074+mplekunov@users.noreply.github.com>
2022-09-21 11:26:54 -0400

    Remove debug statements

--- API/contacts/SearchContact.php
+++ API/contacts/SearchContact.php
@@ -1,7 +1,4 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('error_reporting', E_ALL);
-
     require_once __DIR__ . '/../utils/JsonUtils.php';
     require_once __DIR__ . '/../utils/ResponseSender.php';
     require_once __DIR__ . '/../utils/RequestReceiver.php';
